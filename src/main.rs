96967c2e2d9f7d756a8ec68ebcde942592d177be
damianmyn <damian.mynhardt@gmail.com>
2025-10-05 21:04:43 +0200

    hash logic w/ input

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,35 @@
+use std::{fs::File, io::{self, stdin, stdout, Read, Write}};
+use sha2::{Sha256, Digest};
+
+fn get_file_hash(filepath: &str) -> io::Result<String> {
+    let mut file = File::open(filepath)?;
+    let mut hasher = Sha256::new();
+    let mut buffer = [0; 1024];
+    loop {
+        let bytes_read = file.read(&mut buffer)?;
+        if bytes_read == 0 {
+            break;
+        }
+        hasher.update(&buffer[..bytes_read]);
+    }
+    let hash_result = hasher.finalize();
+    Ok(format!("{:x}", hash_result))
+}
+
+
+
+
 fn main() {
-    println!("Hello, world!");
+    print!("Input file path: ");
+    stdout().flush();
+    let mut input_string: String     = String::new();
+
+    stdin().read_line(&mut input_string);
+
+    let filepath = input_string.trim(); 
+
+    match get_file_hash(filepath) {
+        Ok(hash) => println!("SHA256 | {}", hash),
+        Err(e) => eprint!("Error computing hash: {}", e),
+    };
 }
